7923e7d9bb9e7a978c0858b303a642be6e7e2a26
Justin Silvestre <justinsilvestre@gmail.com>
2024-09-14 22:45:25 +0200

    Fix Guangyun table entries

--- app/features/qysInfo/GuangyunRhymesTable.tsx
+++ app/features/qysInfo/GuangyunRhymesTable.tsx
@@ -6,7 +6,7 @@ const RHYMES =
   `東-tōng 董-tōngˬ 送-sōngˎ 屋-ʾwōk / 翁-ʾwōng-(with_initial_影) 中-tiūng-(三等) 風-pūng-(三等,_labial_initials) 縮-ṣyūk-(入聲,_with_三等_and_二等_sibilants) 肉-nźwīk-(入聲三等) 福-pūk-(入聲,_labial_initials) 育-ẁīk
 冬-tong 湩-tongˬ 宋-songˎ 沃-ʾok
 鍾-tśŷong 腫-tśŷongˬ 用-ŷongˎ 燭-tśŷok
-江-kạ̊ng 講-kạ̊ngˬ 絳-kạ̊ngˎ 覺-kạuˎ
+江-kạ̊ng 講-kạ̊ngˬ 絳-kạ̊ngˎ 覺-kạ̊k
 支-tśï 紙-tśïˬ 寘-tśïˎ / 祇-gyï-(重紐四等) 危-ngwï-(合口) 隓-khẁï-(重紐四等合口) 吹-tśʻuï-(合口,_bimoraic_in_漢音)
 脂-tśī 旨-tśīˬ 至-tśīˎ / 伊-ʾyī-(重紐四等) 龜-kwī-(合口) 葵-gẁī-(重紐四等合口) 追-tuī-(合口,_漢音_vowel_alternation)
 之-tśi 止-tśiˬ 志-tśiˎ
@@ -28,14 +28,14 @@ const RHYMES =
 臻-tsịn 櫛-tsịt
 文-mun 吻-munˬ 問-munˎ 物-mut
 欣-khin 隱-ʾinˬ 焮-khinˎ 迄-khit
-元-ngwên 阮-ngwên 願-ngwênˎ 月-ngwêt / 言-ngên-(開口) 飜-pʻân-(labial_initials)
+元-ngwên 阮-ngwênˬ 願-ngwênˎ 月-ngwêt / 言-ngên-(開口) 飜-pʻân-(labial_initials)
 先-sen 銑-senˬ 霰-senˎ 屑-swot / 淵-ʾwen-(合口)
 仙-sėn 獮-sėnˬ 線-sėnˎ 薛-sėt / 篇-pʻyen-(重紐四等) 全-dzwėn-(合口) 娟-ʾẁen-(重紐四等合口)
 魂-ghwon 混-ghwonˬ 慁-ghwonˎ 沒-mwot
 痕-ghon 很-ghonˬ 恨-ghonˎ 麧-ghot
 寒-ghan 旱-ghanˬ 翰-ghan 曷-ghat
 桓-ghwan 緩-ghwanˬ 換-ghwanˎ 末-mat
-刪-sạn 潸-sạn 諫-kạnˎ 鎋-ghạt / 撰-dzwạnˬ-(合口)
+刪-sạn 潸-sạnˬ 諫-kạnˎ 鎋-ghạt / 撰-dzwạnˬ-(合口)
 山-sạ̈n 産-sạ̈nˬ 襉-kạ̈nˎ 黠-ghạ̈t
 蕭-seu 篠-seuˬ 嘯-seuˎ
 宵-sėu 小-sėuˬ 笑-sėuˎ / 要-ʾyeu-(重紐四等)
@@ -46,7 +46,7 @@ const RHYMES =
 麻-mạ 馬-mạˬ 禡-mạˎ / 遮-tśyạ-(三等) 瓜-kwạ-(合口)
 陽-yang 養-yangˬ 漾-yangˎ 藥-yak / 房-bâng-(labial_and_retroflex_sibilant_initials) 王-wâng-(initial_from_影云以) 狂-gŷang-(合口)
 唐-dang 蕩-dangˬ 宕-dangˎ 鐸-dak / 荒-khwang-(合口)
-庚-kạng 梗-kạngˬ 映-ʾẹngˬ 陌-mạk / 諻-khwạng-(合口) 明-mẹng-(三等) 兄-khwẹng-(三等合口)
+庚-kạng 梗-kạngˬ 映-ʾẹngˎ 陌-mạk / 諻-khwạng-(合口) 明-mẹng-(三等) 兄-khwẹng-(三等合口)
 耕-kạ̈ng 耿-kạ̈ngˬ 諍-tsạ̈ngˎ 麥-mạ̈k / 宏-ghwạ̈ng-(合口)
 清-tsʻėng 靜-dzėngˬ 勁-kyengˎ 昔-sėk / 名-myeng-(重紐四等) 騂-swėng-(合口) 傾-kẁeng-(重紐四等合口)
 青-tsʻeng 迥-ghwengˬ 徑-kengˎ 錫-sek / 熒-ghweng-(合口)
@@ -61,7 +61,7 @@ const RHYMES =
 鹽-yem 琰-yemˬ 豔-yemˎ 葉-yep / 廉-lėm-(三等)
 添-tʻem 忝-tʻemˬ 㮇-tʻemˎ 怗-tʻep
 咸-ghạ̈m 豏-ghạ̈mˬ 陷-ghạ̈mˎ 洽-ghạ̈p
-銜-ghạm 檻-ghạmˬ 鑑-kạm 狎-ghạp
+銜-ghạm 檻-ghạmˬ 鑑-kạmˎ 狎-ghạp
 嚴-ngêm 儼-ngêmˬ 釅-ngêmˎ 業-ngêp
 凡-bâm 范-bâmˬ 梵-bâmˎ 乏-bâp`
     .split("\n")
