095a729c994f3bf9b89ee8ef02ab5eb773740899
Justin Silvestre <justinsilvestre@gmail.com>
2024-11-13 15:15:08 +0100

    Lint

--- prisma/kanjisense/seedKanjisenseFigureRelation.ts
+++ prisma/kanjisense/seedKanjisenseFigureRelation.ts
@@ -1,4 +1,4 @@
-import { KanjiDbVariantType, PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 import { getSeedInterface } from "prisma/SeedInterface";
 import {
