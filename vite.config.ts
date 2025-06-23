a901089a39dc2a86a313940cd972f18ef8f09bf6
Justin Silvestre <justinsilvestre@gmail.com>
2025-06-23 15:32:13 +0200

    Move glyphs and shuowenSvgs assets out of app directory 3

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       watch: {
-        exclude: ["**/app/lib/vendor/**", "**/app/lib/dic/glyphs/**"],
+        exclude: ["**/assets/**"],
       },
     },
   },
