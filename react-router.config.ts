6129eadd416c8b84bbdb369d5d567a400f071f55
Justin Silvestre <justinsilvestre@gmail.com>
2025-06-23 11:00:01 +0200

    Prerender only sound components page

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  prerender: [
-    "/browse/sound-components",
-    "/browse/atomic-components",
-    "/browse/compound-components",
-    "/dict/middle-chinese",
-    "/dict/middle-chinese-pronunciation",
-  ],
+  prerender: ["/browse/sound-components"],
 } satisfies Config;
