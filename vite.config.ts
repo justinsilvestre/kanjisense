634c9ea037fd06b04183216613ed1e2896b426d5
Justin Silvestre <justinsilvestre@gmail.com>
2025-06-23 15:00:25 +0200

    Try different glob pattern for vite config build watch exclude

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   build: {
     watch: {
-      exclude: ["./app/lib/vendor/**", "./app/lib/dic/glyphs/**"],
+      exclude: ["**/app/lib/vendor/**", "**/app/lib/dic/glyphs/**"],
     },
   },
   plugins: [reactRouter(), tsconfigPaths()],
